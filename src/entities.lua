e693ee10e5abcd4884f6f15f87c8357780ea3191
MarcusGH <29378769+marcusGH@users.noreply.github.com>
2019-12-06 15:16:58 +0000

    added collision detection

---
+++ src/entities.lua
@@ -0,0 +1,78 @@
+local g = love.graphics
+
+local Entities = {}
+
+-- A list of the entities we have
+Entities.entities = {}
+
+-- create a dicitonary of the entity fields
+local function addEntity(posX, posY, speedX, speedY, width, height)
+	local entity = {}
+	entity.x = posX
+	entity.y = posY
+	entity.speedX = speedX
+	entity.speedY = speedY
+	entity.width = width
+	entity.height = height
+
+	table.insert(Entities.entities, entity)
+end
+
+-- Collision detection function;
+-- Returns true if two boxes overlap, false if they don't;
+-- x1,y1 are the top-left coords of the first box, while w1,h1 are its width and height;
+-- x2,y2,w2 & h2 are the same, but for the second box.
+local function CheckEntityCollision(entity1, entity2)
+  return entity1.x < entity2.x+entity2.width and
+         entity2.x < entity1.x+entity1.width and
+         entity1.y < entity2.y+entity2.height and
+         entity2.y < entity1.y+entity1.height
+end
+
+--are there any collisions?
+local function checkCollision(player)
+	for i=1,#Entities.entities do
+		if CheckEntityCollision(Entities.entities[i], player) == true then
+			return true
+		end
+	end
+	return false
+end
+
+--make it public
+Entities.checkCollision = checkCollision
+
+-- draw one particular entity
+local function drawEntity(entity)
+	love.graphics.rectangle("fill", entity.x, entity.y, entity.width, entity.height)
+end
+
+-- main drawing function
+local function draw()
+	for i=1,#Entities.entities do
+		drawEntity(Entities.entities[i])
+	end
+end
+
+--make it public
+Entities.draw = draw
+
+local function updateEntity(entity, dt)
+	entity.x = entity.x + entity.speedX * dt
+	entity.y = entity.y + entity.speedY * dt
+end
+
+local function updateEntities(dt)
+	for i=1,#Entities.entities do
+		updateEntity(Entities.entities[i], dt)
+	end
+end
+
+--make it public
+Entities.update = updateEntities
+
+-- --------------------------------------------
+addEntity(500, 50, -80, 0, 10, 30)
+addEntity(300, 200, -40, 10, 70, 10)
+
+return Entities
