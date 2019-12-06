414d7e13b24e93fefccb83ae64152a23e2b00aa5
MarcusGH <29378769+marcusGH@users.noreply.github.com>
2019-12-06 19:46:26 +0000

    Added splash

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,7 @@
+	gameStarted = false
+
 function love.load()
+
 	g = love.graphics
 	k = love.keyboard
 
@@ -20,44 +23,57 @@ function love.load()
 end
 
 function love.draw()
-	background.draw()
-	hud.draw(character.score, 1000)
-	entities.draw()
-	character.draw()
+	if gameStarted then
+		background.draw()
+		hud.draw(character.score, 1000)
+		entities.draw()
+		character.draw()
+	else
+		img = love.graphics.newImage("resources/splash.png")
+		love.graphics.draw(img, 0, 0)
+	end
 end
 
 function love.update(dt)
 
-	if timer > 0 and distanceTravelled < distanceRequired then
+	if gameStarted then
 
-		if k.isDown("s") then
-			character.study(dt)
-		else
-			character.notStudy()
+		if timer > 0 and distanceTravelled < distanceRequired then
+
+			if k.isDown("s") then
+				character.study(dt)
+			else
+				character.notStudy()
+			end
+
+			background.update(character.speedX, dt)
+			hud.update()
+			entities.update(dt, character.speedX)
+
+			if entities.checkCollision(character) then
+				character.stumble()
+			end
+			
+			if k.isDown("down") then
+				character.slide(dt)
+			elseif k.isDown("up") then
+				character.jump(dt)
+			else
+				character.noInputNextState(dt)
+			end
+
+			timer =  timer - dt
+
 		end
 
-		background.update(character.speedX, dt)
-		hud.update()
-		entities.update(dt, character.speedX)
-
-		if entities.checkCollision(character) then
-			character.stumble()
+		if k.isDown('r') then
+			--gameStarted = false
+			love.load()
 		end
-		
-		if k.isDown("down") then
-			character.slide(dt)
-		elseif k.isDown("up") then
-			character.jump(dt)
-		else
-			character.noInputNextState(dt)
+	else
+		if k.isDown('r') then
+			gameStarted = true
 		end
-
-		timer =  timer - dt
-
-	end
-
-	if k.isDown('r') then
-		love.load()
 	end
 
 end
